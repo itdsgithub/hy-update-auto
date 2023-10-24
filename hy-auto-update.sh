08d73fd7ea747874db82497648cff8f05d54e7c1
itdsgithub <itdsgg@gmail.com>
2023-10-24 20:05:35 +0800

    Update hy-auto-update.sh

--- hy-auto-update.sh
+++ hy-auto-update.sh
@@ -13,19 +13,23 @@ latest_version=$(curl -sI -w %{url_effective} -L -s "${releases_url}" -o /dev/nu
 current_version=$(hysteria version | awk '/Version:/ {print $2}')
 
 # 输出当前版本号
-echo "当前版本号为：${current_version}"
+echo "当前安装的版本号为：${current_version}"
 
 # 比较版本号
 if [[ "${latest_version}" != "${current_version}" ]]; then
+    echo "发现新版本：${latest_version}"
+    echo "正在下载最新版本的 hysteria-linux-amd64 文件..."
+
     # 下载最新版本的 hysteria-linux-amd64 文件
-    wget -O "/usr/local/bin/${asset_name}.new" "${releases_url}/${asset_name}"
+    wget -q -O "/usr/local/bin/${asset_name}.new" "${releases_url}/${asset_name}"
 
     # 替换当前版本的 hysteria 文件
     mv "/usr/local/bin/${asset_name}.new" "/usr/local/bin/${asset_name}"
 
-    # 输出更新信息
-    echo "已更新到版本 ${latest_version}"
+    #重启hysteria核心
+     systemctl restart hysteria-server.service
+
+    echo "已成功更新到版本 ${latest_version}"
 else
-    # 输出取消信息
-    echo "无需更新，当前已是最新版本"
+    echo "当前已是最新版本，无需更新"
 fi
